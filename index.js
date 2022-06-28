49c887737c75db18c21d91aed3c5a2207fb6c45f
Annu Kumar Singh <annukumarsingh.aks@gmail.com>
2022-06-28 23:50:46 +0530

    heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express= require('express');//step1
-const port=8000;//step1
+// const port=8000;//step1
 const app= express();  //step1
 const ejs = require('ejs');
 const path = require('path');
+const PORT = process.env.PORT || 8000;
 
 
 
@@ -10,7 +12,6 @@ const path = require('path');
 // adding ejs to my views step 5
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
 
 
 
@@ -22,10 +23,10 @@ app.use('*',(req,res)=>{
 })
 
 // listening on port
-app.listen(port,(err)=>{//step1
+app.listen(PORT,(err)=>{//step1
     if(err){
         console.log('Error in running server',err);
         return;
     }
-    console.log("Server is running on port",port);
+    console.log("Server is running on port",PORT);
 });
